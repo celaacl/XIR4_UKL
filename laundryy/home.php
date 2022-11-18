dffa0ef3f136bfffe93931940678b835499f6df6
cecell <91251956+celaacl@users.noreply.github.com>
2022-11-18 09:57:55 +0700

    Add files via upload

---
+++ laundryy/home.php
@@ -0,0 +1,14 @@
+<?php
+include "header.php";
+?>
+<div class="page-banner home-banner">
+  <div class="container h-100">
+    <div class="row align-items-center h-100">
+      <div class="col-lg-6 py-3 wow fadeInUp">
+        <h4><strong>Laun<span class="text-primary">Dry</span></strong></h4>
+        <p class="text-lg mb-5">Web Laundry yang dibuat oleh Clarynta Ve Davine.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<html>
